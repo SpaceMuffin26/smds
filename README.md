978deeaf69958f0073ad24be47f41835cfe27028
s.diasamidze <saba2006.diasamidze@gmail.com>
2025-01-04 16:01:46 +0400

    added a basic queue implementation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# smds
-A basic Go library for stacks, queues, and other simple data structures .
+# smds - Space Muffin's Data structures
+
+<---- Stack ---->
+
+
---
+++ queue.go
@@ -0,0 +1,19 @@
+package main
+
+type Queue struct {
+	list []int
+}
+
+func (pqueue *Queue) Enqueue(element int) {
+	pqueue.list = append(pqueue.list, element)
+}
+
+func (pqueue *Queue) Dequeue() (element int, isempty bool) {
+	if len(pqueue.list) == 0 {
+		return 0, false
+	} else {
+		passBack := pqueue.list[0]
+		pqueue.list = pqueue.list[1:]
+		return passBack, true
+	}
+}
